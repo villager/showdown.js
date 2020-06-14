ec784976af29d559dd3a81fb58ece31a83a58b2a
Nicolás Serna <nicolito128@hotmail.com>
2020-06-14 01:52:42 -0300

    Update client-user

--- src/structures/client-user.js
+++ src/structures/client-user.js
@@ -1,3 +1,5 @@
+"use strict";
+
 class User {
     constructor(data, client) {
         this.name = "";
@@ -6,23 +8,40 @@ class User {
         this.rooms = new Set();
         this.lastMessage = null;
         this.lastSeen = null;
+        /** @type {string | number} */
+        this.avatar = null;
+        this.idle = false;
+        this.status = "";
+
         this.init(data);
         Object.defineProperty(this, 'client', {value: client});
-
     }
+
     get id() {
         return this.name;
     }
+
     update() {
         this.lastSeen = Date.now();
     }
+
+    /**
+     * 
+     * @param {object} data 
+     * @return {void}
+     */
     init(data) {
         if (data.name) this.name = data.name;
         if (data.group) this.group = data.group;
+        this.avatar = data.avatar || 1;
+        this.status = data.status || "";
         this.connected = true;
         this.lastMessage = "";
+        this.idle = false;
         this.update();
     }
+
+    /** @return {JSON} */
     toJSON() {
         let json = Object.create(null);
         for (const [key, value] of this) {
