e628457a66867f09fe3eef296c0f64700c85d099
Aldair Beltran <aldairbeltran3@hotmail.com>
2020-06-14 00:27:44 -0500

    Document users

--- src/structures/user.js
+++ src/structures/user.js
@@ -1,35 +1,31 @@
 class User {
-    constructor(data, client) {
-        this.name = "";
-        this.group = " ";
-        this.connected = false;
-        this.rooms = new Set();
-        this.lastMessage = null;
-        this.lastSeen = null;
-        this.init(data);
-        Object.defineProperty(this, 'client', {value: client});
+    /**
+     * Class for the bot as user
+     * @param {Client} client 
+     */
+    constructor() {
+        /** @type {string} */
+        this.group = ' ';
+        /** @type {Boolean} */
+        this.idle = false;
+        /** @type {string?} */
+        this.status = '';
     }
+    /** 
+     * Gets the bot Id
+     * @return {string}
+    */
     get id() {
         return this.name;
     }
-    update() {
-        this.lastSeen = Date.now();
-    }
-    init(data) {
-        if (data.name) this.name = data.name;
+    /**
+     * Update Bot's info
+     * @param {AnyObject} data 
+     * @return {void}
+     */
+    update(data) {
         if (data.group) this.group = data.group;
-        this.connected = true;
-        this.lastMessage = "";
-        this.update();
     }
-    toJSON() {
-        let json = Object.create(null);
-        for (const [key, value] of this) {
-            if(typeof this[key] !== "function") {
-                json[key] = value;
-            }
-        }
-        return json;
-    }
+    
 }
 module.exports = User;
