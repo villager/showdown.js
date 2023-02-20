e3faae3fd47971f7f6f20cbcf9f7ef59c973a585
Bema Bemel <aldair.beltran@outlook.com>
2023-02-19 20:24:35 -0600

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 
 export {Client} from './client';
+export {User, Room, MessageHandler} from './structures';
+export {UserManager,RoomManager, FormatsManager, ConnectionManager, ChatManager, LoginManager, SendManager} from './managers';
+export {Net} from './lib';
